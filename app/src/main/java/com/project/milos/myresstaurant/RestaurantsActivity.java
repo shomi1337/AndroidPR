f22181e38ad02fad45a0d203758c1970723fdd84
Milos Novakovic <novakovic.va@gmail.com>
2017-01-20 02:42:03 +0100

    Commit

--- app/src/main/java/com/project/milos/myresstaurant/RestaurantsActivity.java
+++ app/src/main/java/com/project/milos/myresstaurant/RestaurantsActivity.java
@@ -3,12 +3,19 @@ package com.project.milos.myresstaurant;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridView;
@@ -21,7 +28,7 @@ import java.util.List;
  * Created by Miloš on 1/11/2017.
  */
 
-public class RestaurantsActivity extends AppCompatActivity {
+public class RestaurantsActivity extends AppCompatActivity  {
     private GridView gridRestaurants;
     public static ArrayList<String> ArrayofName = new ArrayList<String>();
     RestaurantsAdapter rAdapter;
